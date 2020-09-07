e5474a029cdf66e4e5b72ca6d8dd02a20b40f883
hiltas73 <h_iltas@yahoo.com>
2020-09-07 20:25:12 +0300

    first hiltas73

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/cybertek/step_definitions",
 
-        dryRun = false,
+        dryRun = true,
         tags = "@calendar"
 
 
