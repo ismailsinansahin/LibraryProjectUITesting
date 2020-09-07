63ebea64954b6ea2da9180b3d1cadfc6e01df6d7
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-09-07 19:32:50 +0200

    update Hook.java

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -16,7 +16,7 @@ public class Hooks {
     @Before
     public void setUp(){
         Driver.get().manage().window().maximize();
-        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.get().manage().timeouts().implicitlyWait(11, TimeUnit.SECONDS);
     }
 
     @After
